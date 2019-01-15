258ccf3b334955f22b5949104d6aae30422c7997
street <a289237642@163.com>
2019-01-15 09:20:30 +0800

    Update pipelines.py

--- fhw/fhw/pipelines.py
+++ fhw/fhw/pipelines.py
@@ -10,34 +10,6 @@ import pymysql, requests
 from scrapy.conf import settings
 from datetime import datetime
 
-PREFIX = r'Z:\\'
-WEBSITE = r'ifeng'
-
-"""
- filename = os.path.join(PREFIX, WEBSITE, urlpath.strip('/'))
-        file_dir = os.path.dirname(filename)
-        os.makedirs(file_dir, exist_ok=True)
-        with open(filename, 'w', encoding='utf-8', errors='ignore') as f:
-            f.write(driver.page_source)
-"""
-
-
-# class ImagePipeline(object):
-#     def process_item(self, item, spider):
-#         # 图片下载
-#         path_url = item['TP_URL']
-#         print("===", path_url)
-#         if not os.path.exists("PREFIX/Images/"):
-#             os.makedirs("PREFIX/Images/")
-#         image_path = "PREFIX/Images/" + path_url.split("/")[-1]
-#
-#         if not os.path.exists(image_path):
-#             response = requests.get(path_url)
-#             if response.status_code == 200:
-#                 with open(image_path, "wb")as f:
-#                     f.write(response.content)
-#         return item
-
 
 class FhwPipeline(object):
     def __init__(self):
@@ -51,30 +23,30 @@ class FhwPipeline(object):
         self.cursor = self.connect.cursor()
 
     def process_item(self, item, spider):
-        # print("=======item", item)
 
         # 图片下载
         path_url = item['TP_URL']
         print("===", path_url)
-        if not os.path.exists(PREFIX+"/Images/"):
-            os.makedirs(PREFIX+"/Images/")
-        image_path = PREFIX+"/Images/" + path_url.split("/")[-1]
+        WEBSITE = r'ifeng/' + item['LMLJ2'] + ''
+        if not os.path.exists(WEBSITE + "/images/"):
+            os.makedirs(WEBSITE + "/images/")
+        image_path = WEBSITE + "/images/" + path_url.split("/")[-1]
 
         if not os.path.exists(image_path):
             response = requests.get(path_url)
             if response.status_code == 200:
                 with open(image_path, "wb")as f:
                     f.write(response.content)
-
-        try:
-            self.cursor.execute(
-                "insert into news(BTIT,CYRS,PLS,XWLY,LMLJ,ZWWB,ZWNR,TP_URL,BZ,TJS,CJSJ,YS_URL,CL_URL,RKSJ,CGSJ) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
-                [item['BTIT'], item['CYRS'], item['PLS'], item['XWLY'], item['LMLJ'], item['ZWWB'], item['ZWNR'],
-                 image_path, item['BZ'], item['TJS'], item['CJSJ'], item['YS_URL'], item['CL_URL'], datetime.now(),
-                 item['CGSJ']])
-            self.connect.commit()
-        except Exception as error:
-            print(error)
+        self.connect.ping(reconnect=True)
+        self.cursor.execute(
+            "insert into lg2(BTIT,CYRS,PLS,XWLY,LMLJ,BZ,CGSJ,CJSJ,ZWWB,ZWNR,TJS,YS_URL,CL_URL,TP_URL,RKSJ,URL) "
+            "values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+            [item.get('BTIT'), item.get('CYRS'), item.get('PLS'), item.get('XWLY'), item.get('LMLJ'), item.get('BZ'),
+             item.get('CGSJ'),
+             item.get('CJSJ'), item.get('ZWWB'), item.get('ZWNR'), item.get('TJS'), item.get('YS_URL'), item['CL_URL'],
+             image_path,
+             datetime.now(), item['URL']])
+        self.connect.commit()
         return item
 
     def close_spider(self, spider):
